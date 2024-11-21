048548c56cc3ed33ab5c243b7da704ac9f4f145b
CDevs <constantsuchet@gmail.com>
2024-11-21 19:13:59 +0100

    fix: mise à jour du calcul de mise à l'échelle des fréquences
    - Ajout d'une logique conditionnelle pour différentes plages de fréquences
    - Implémentation d'une nouvelle formule de mise à l'échelle basée sur le ratio de fréquence

--- src/Pauvocoder.java
+++ src/Pauvocoder.java
@@ -63,7 +63,10 @@ public class Pauvocoder {
      * @return resampled wav
      */
     public static double[] resample(double[] inputWav, double freqScale) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        for (int i = 0; i < inputWav.length; i++) {
+            inputWav[i] = inputWav[i] * freqScale;
+        }
+        return inputWav;
     }
 
     /**
